ed1d7b78bef629139e72d4e9c6d0b53f69759abf
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2015-12-13 11:05:49 -0800

    JS edits

---
+++ bus_mall/js/app.js
@@ -0,0 +1,67 @@
+// Object Constructor: to manage photos
+// Declare variable(s)
+// Put objects in an array
+var randomImage = new Object();
+
+randomImage.randNum = function(min,max) {
+  return Math.floor(Math.random() * (max - min) + parseInt(min));
+}
+randomImage.rankArray = ['image1', 'image2', 'image3'];
+randomImage.imageArray = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
+randomImage.maxImages = 14;
+
+
+// rankArray.push(this);
+//Put the random number in a function
+//Generate a random #
+
+
+function randNum(min, max) {
+  return Math.random() * (maxImages - 1) + parseInt(1);
+  console.log(random.Image.randNum);
+
+  }
+
+
+
+
+
+//
+// //Do this two more times
+// //if != el.first else run the function again
+//
+// //Get three objects and display them
+
+var randomImage = new Image();
+var div = document.getElementById('vote');
+
+img.onload = function() {
+  div.appendChild(image1);
+  div.appendChild(image2);
+  div.appendChild(image3);
+};
+
+img.src = 'img/';
+//
+//
+// //Use an event handler
+function(displayThree)() {
+  var elVote = document.getElementById('VOTE');
+  if ()
+}
+
+// //Register a 'submit'
+// //Trace the 'submit' back to the object
+var el = document.getElementById('vote');
+if (el.addEventListener)
+    el.addEventListener('vote', doFunction, false);
+else if (el.attachEvent)
+    el.attachEvent('onclick', doFunction);
+// //Write 3 event handlers and write listeners
+//
+// //After further analysis, you realize that you'll need the following domain models:
+//
+//   //A set of Product objects to manage all the attributes and behaviors around the product photos... hmm... how to make multiple objects easily?
+//   //A single ranker object to manage all the attributes and behaviors around a visitor's product-ranking experience.
+
+// busMall();
